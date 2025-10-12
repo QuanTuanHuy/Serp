2e2676a9c960036863075482a8c26e64b013832c
QuanTuanHuy <quan.tuan.huy@gmail.com>
2025-10-12 19:18:05 +0700

    feat(SA): initial roles & super admin

--- account/src/main/java/serp/project/account/core/service/impl/KeycloakUserService.java
+++ account/src/main/java/serp/project/account/core/service/impl/KeycloakUserService.java
@@ -42,6 +42,7 @@ public class KeycloakUserService implements IKeycloakUserService {
     @Override
     public void assignRealmRoles(String userId, List<String> roleNames) {
         try {
+            log.info("Rolenames: {}", roleNames);
             keycloakPort.assignRealmRoles(userId, roleNames);
         } catch (AppException e) {
             log.error("Error assigning realm roles to user {}: {}", userId, e.getMessage());
