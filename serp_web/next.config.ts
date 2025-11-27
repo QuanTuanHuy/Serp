92ea65eb2c691a3ff9620a2bf4f447d498705a0c
Quan Huy <quan.tuan.huy@gmail.com>
2025-11-27 12:26:11 +0700

    feat(web): optimize docker

--- serp_web/next.config.ts
+++ serp_web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone', // Enable standalone output for optimized Docker builds
 };
 
 export default nextConfig;
