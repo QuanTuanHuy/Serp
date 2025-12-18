c0578d84f23fb2d08e70eba1669b55ef4516c9b0
kiencoam <kienpham1392004@gmail.com>
2025-12-18 08:55:01 +0700

    update purchase service

--- purchase_service/src/main/java/serp/project/purchase_service/repository/OrderItemBillingRepository.java
+++
@@ -1,7 +0,0 @@
-package serp.project.purchase_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import serp.project.purchase_service.entity.OrderItemBillingEntity;
-
-public interface OrderItemBillingRepository extends JpaRepository<OrderItemBillingEntity, String> {
-}
