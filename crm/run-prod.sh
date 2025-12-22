a1ac850b305f249fa0b4b4f47d57873963a55dc0
Quan Huy <quan.tuan.huy@gmail.com>
2025-12-22 08:31:31 +0700

    add prod run script

--- crm/run-prod.sh
+++ crm/run-prod.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Author: QuanTuanHuy, Description: Part of Serp Project
 
-echo "Loading environment variables from .env file..."
-
 if [ -f .env.prod ]; then
   set -a
   source <(sed -e 's/^\s*export\s\+//g' -e 's/\r$//g' .env.prod)
@@ -10,9 +8,7 @@ if [ -f .env.prod ]; then
 fi
 
 echo ""
-echo "Starting Account Service..."
+echo "Starting CRM in production mode..."
 echo ""
-
-export SPRING_PROFILES_ACTIVE=prod
 
 ./mvnw spring-boot:run -Dspring-boot.run.profiles=prod
