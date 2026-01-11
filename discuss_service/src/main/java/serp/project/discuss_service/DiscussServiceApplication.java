55f343b6d2a8174a385d1be7eca725897df53790
QuanTuanHuy <quan.tuan.huy@gmail.com>
2026-01-12 01:34:26 +0700

    init discuss service

---
+++ discuss_service/src/main/java/serp/project/discuss_service/DiscussServiceApplication.java
@@ -0,0 +1,13 @@
+package serp.project.discuss_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscussServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscussServiceApplication.class, args);
+	}
+
+}
