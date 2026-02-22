3e7038ab4ddb59a011d2ce70dcff8139d70d772f
QuanTuanHuy <quan.tuan.huy@gmail.com>
2026-02-22 17:22:29 +0700

    migrate pmcore to java

---
+++ pm_core/src/main/java/serp/project/pmcore/PmcoreApplication.java
@@ -0,0 +1,13 @@
+package serp.project.pmcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmcoreApplication.class, args);
+	}
+
+}
