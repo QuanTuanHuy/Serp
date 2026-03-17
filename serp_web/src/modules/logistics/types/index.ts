80c3ffce7a8d9e29b6845958764ec1c388aceea9
kiencoam <kienpham1392004@gmail.com>
2026-03-17 21:37:56 +0700

    [logistics] fix type

--- serp_web/src/modules/logistics/types/index.ts
+++ serp_web/src/modules/logistics/types/index.ts
@@ -6,10 +6,10 @@ Description: Part of Serp Project - Logistics Module Types
 // Constants
 export type ResponseStatus = 'SUCCESS' | 'FAILED';
 export type EntityType = 'PRODUCT' | 'SUPPLIER' | 'CUSTOMER' | 'FACILITY';
-export type AddressType = 'FACILIY' | 'SHIPPING' | 'BUSSINESS';
+export type AddressType = 'FACILITY' | 'SHIPPING' | 'BUSINESS';
 export type CustomerStatus = 'ACTIVE' | 'INACTIVE';
 export type FacilityStatus = 'ACTIVE' | 'INACTIVE';
-export type InventoryItemStatus = 'VALID' | 'EXPIRED' | 'DAMAGED';
+export type InventoryItemStatus = 'VALID' | 'INVALID';
 export type OrderItemStatus = 'CREATED' | 'DELIVERED';
 export type OrderStatus =
   | 'CREATED'
