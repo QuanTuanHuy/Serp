eb6f6c9342fae2007e0f7c38882a29ffc2e1ace0
ntabodoiqua <anhnta2004@gmail.com>
2026-03-24 22:19:20 +0700

    feat(tms): initialize first-mile module with Spring Boot setup, domain entities, configs, and database scripts

---
+++ first-mile/src/main/java/serp/project/first_mile/FirstMileApplication.java
@@ -0,0 +1,13 @@
+package serp.project.first_mile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMileApplication.class, args);
+	}
+
+}
---
+++ first-mile/src/test/java/serp/project/first_mile/FirstMileApplicationTests.java
@@ -0,0 +1,13 @@
+package serp.project.first_mile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstMileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
