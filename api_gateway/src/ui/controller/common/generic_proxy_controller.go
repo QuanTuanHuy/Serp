403d4a68e8099659ab42e1d97c0822fce37b3432
Quản Tuấn Huy <quan.tuan.huy@gmail.com>
2026-05-03 21:49:20 +0700

    feat(pm): add pmCoreService configuration and routing
    - Introduced pmCoreService configuration in local and production YAML files, specifying host, port, and timeout settings.
    - Updated ExternalServiceProperties struct to include pmCoreService.
    - Enhanced generic proxy controller to support routing for pmCoreService.
    - Created a new router for pmCoreService with JWT authentication and rate limiting.
    
    This commit establishes the necessary infrastructure for integrating pmCoreService into the API gateway.

--- api_gateway/src/ui/controller/common/generic_proxy_controller.go
+++ api_gateway/src/ui/controller/common/generic_proxy_controller.go
@@ -130,6 +130,12 @@ func buildServiceRoutes(props *properties.ExternalServiceProperties) []ServiceRo
 			TargetPrefix: "/discuss/api/v1",
 			Target:       props.DiscussService.BaseURL(),
 		},
+		{
+			Name:         "pm-core",
+			SourcePrefix: "/pm/api/v1",
+			TargetPrefix: "/pm-core/api/v1",
+			Target:       props.PmCoreService.BaseURL(),
+		},
 	}
 }
 
