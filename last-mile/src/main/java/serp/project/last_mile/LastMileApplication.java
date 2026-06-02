604dac05bb0a3dc3af794b2cd6cf18703c749aab
ntabodoiqua <anhnta2004@gmail.com>
2026-06-02 17:26:33 +0700

    feat(tms): add initial implementation of Last-Mile service with configuration files and application structure

---
+++ last-mile/src/main/java/serp/project/last_mile/LastMileApplication.java
@@ -0,0 +1,54 @@
+/*
+Author: Nguyen The Anh
+Description: Part of Serp Project
+*/
+package serp.project.last_mile;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableScheduling
+public class LastMileApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(LastMileApplication.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(LastMileApplication.class, args);
+        printApplicationInfo(context.getEnvironment());
+    }
+
+    private static void printApplicationInfo(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (contextPath == null || contextPath.isBlank()) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+
+        System.out.println("\n----------------------------------------------------------\n\t" +
+                "Application is running! Access URLs:\n\t" +
+                "Local: \t\t" + protocol + "://localhost:" + serverPort + contextPath + "\n\t" +
+                "External: \t" + protocol + "://" + hostAddress + ":" + serverPort + contextPath + "\n" +
+                "----------------------------------------------------------");
+    }
+}
