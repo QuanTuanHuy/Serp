7e525e8140db872776fbf6e18ba17098825fc39f
ntabodoiqua <anhnta2004@gmail.com>
2026-07-10 14:46:40 +0700

    feat(tms): fix test for payment-service

--- tms-payment-service/src/test/java/serp/project/tms_payment_service/PaymentServiceApplicationTests.java
+++ tms-payment-service/src/test/java/serp/project/tms_payment_service/PaymentServiceApplicationTests.java
@@ -2,9 +2,28 @@ package serp.project.tms_payment_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import serp.project.tms_payment_service.scheduler.OrderStatusQueryScheduler;
+import serp.project.tms_payment_service.scheduler.RefundStatusChecker;
+import serp.project.tms_payment_service.scheduler.WebhookRetryScheduler;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:tms_payment_test;MODE=PostgreSQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.hibernate.ddl-auto=none"
+})
 class PaymentServiceApplicationTests {
+
+	@MockBean
+	private OrderStatusQueryScheduler orderStatusQueryScheduler;
+
+	@MockBean
+	private RefundStatusChecker refundStatusChecker;
+
+	@MockBean
+	private WebhookRetryScheduler webhookRetryScheduler;
 
 	@Test
 	void contextLoads() {
